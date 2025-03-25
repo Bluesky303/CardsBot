576ad614d8acf603a17049f418eb66ca9dc16f94
Blue_sky303 <blue_sky303@qq.com>
2025-03-25 17:16:27 +0800

    牌堆优化

--- cardbot.py
+++ cardbot.py
@@ -1,9 +1,9 @@
 import uvicorn
-from contextlib import asynccontextmanager
 from fastapi import FastAPI, Request
-import aiohttp, asyncio
-import time
+import aiohttp
 import cards
+
+from order import handle
 
 app = FastAPI() 
 
@@ -16,39 +16,19 @@ card1 = cards.Card('attack', 'attack', 1, attack(6))
 card2 = cards.Card('defence', 'defence', 2, defence(5))
 Pile = cards.CardPlie([card1]*5+[card2]*5)
 
-def handle(Pile: cards.CardPlie, order: list):
-    dic1 = {
-        '抽牌堆': Pile.show_draw_pile,
-        '手牌': Pile.show_hand_pile,
-        '弃牌堆': Pile.show_discard_pile,
-        '消耗': Pile.show_exhausted_pile,
-    }
-    dic2 = {
-        '抽牌': Pile.draw,
-        '使用': Pile.using,
-        '弃牌': Pile.discard,
-        '搜寻': Pile.search,
-        '回收': Pile.reclaim,
-        }
-    if order[0] in dic1:
-        return dic1[order[0]]()
-    if order[0] in dic2:
-        return dic2[order[0]](int(order[1]))
-    return None
-
 async def printPile(Piledata: tuple, group_id):
+    text = {
+        'text': f'当前{Piledata[0]}:\n' + '\n'.join([str(num) + Piledata[1][num] for num in len(Piledata[1])])
+        }
     async with aiohttp.ClientSession() as session:
         reply = {
             'group_id': group_id,
             'message': [{
                 'type': 'text',
-                'data': {
-                    'text': f'当前{Piledata[0]}:' + ' '.join([card.name for card in Piledata[1]])
-                }
+                'data': text
             }]
         }
         await session.post('http://localhost:3000/send_group_msg', json=reply)
-    
 
 @app.post("/onebot")
 async def root(request: Request):
@@ -62,31 +42,3 @@ async def root(request: Request):
         
 if __name__ == "__main__":
     uvicorn.run(app, port=8070)
-    
-# import cards
-# import random
-# import copy
-# def attack(damage):
-#     return {'attack': damage}
-
-# def defence(shield):
-#     return {'defence': shield}
-
-# def print1(Pile):
-#     for card in Pile:
-#         print(card.name, end=' ')
-#     print()
-
-# if __name__ == "__main__":
-#     card1 = cards.Card('attack', 'attack', 1, attack(6))
-#     card2 = cards.Card('defence', 'defence', 2, defence(5))
-#     Pile = cards.CardPlie([card1]*5+[card2]*5)
-#     print1(Pile.show_draw_pile())
-#     Pile.draw(5)
-#     print1(Pile.show_hand_pile())
-#     Pile.using(2)
-#     print1(Pile.show_hand_pile())
-#     Pile.discard(2)
-#     print1(Pile.show_hand_pile())
-#     print1(Pile.show_discard_pile())
-        
