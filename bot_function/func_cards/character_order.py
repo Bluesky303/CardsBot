b904335718ba1011bf557560dc9b6aebe1b2c862
Blue_sky303 <blue_sky303@qq.com>
2025-04-01 20:23:44 +0800

    1

--- bot_function/func_cards/character_order.py
+++ bot_function/func_cards/character_order.py
@@ -7,7 +7,7 @@ from ..message import *
 from .character import *
 
 async def character_order(order, group_id, user_id):
-    character_dic_list = ['角色状态', '角色列表', '创建角色', '切换角色', '修改角色属性', '添加效果', '删除效果', '添加卡牌']
+    character_dic_list = ['角色状态', '角色列表', '创建角色', '切换角色', '修改角色属性', '添加效果', '删除效果', '卡牌']
     if order[0] in character_dic_list:
         P = Character(group_id, user_id) # 从文件创建角色并及时保存保证可中断
     else:
