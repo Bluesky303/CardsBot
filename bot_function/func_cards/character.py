519cfff72f3d606032c6b868e40b0ead0eca5c99
Blue_sky303 <blue_sky303@qq.com>
2025-04-01 22:48:25 +0800

    1

--- bot_function/func_cards/character.py
+++ bot_function/func_cards/character.py
@@ -287,4 +287,5 @@ class Character:
         return '战斗开始'
     
     def end_battle(self, arg = []): # 结束战斗
+        print(f'del {self.path}{self.characterlist["now"]}/battle.json'.replace('/', '\\'))
         os.system(f'del {self.path}{self.characterlist["now"]}/battle.json'.replace('/', '\\'))
