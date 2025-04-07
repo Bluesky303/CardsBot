5de1fb2c76eb0d1acff4af2dfd20fd4127dcd60a
Blue_sky303 <blue_sky303@qq.com>
2025-04-07 17:31:31 +0800

    1

--- bot_function/func_cards/character_order.py
+++ bot_function/func_cards/character_order.py
@@ -119,12 +119,13 @@ async def character_order(order, group_id, user_id):
             0 打击*5
             1 防御*5
             '''
-            onbattle_dic = {
-                '结束': [at_user(user_id), create_text_msg(' ' + Piledata)], 'end': [at_user(user_id), create_text_msg(' ' + Piledata)],
-                '回合结束': [at_user(user_id), create_text_msg(' 回合结束')], 'ted': [at_user(user_id), create_text_msg(' 回合结束')],
-            }
+            
             Pile_text = [at_user(user_id), create_text_msg(f' 当前{Piledata[0]}:\n' + '\n'.join([str(num) + ' ' + Piledata[1][num]['name'] for num in range(len(Piledata[1]))]))]
             if order[0] in onbattle_dic:
+                onbattle_dic = {
+                    '结束': [at_user(user_id), create_text_msg(' ' + Piledata)], 'end': [at_user(user_id), create_text_msg(' ' + Piledata)],
+                    '回合结束': [at_user(user_id), create_text_msg(' 回合结束')], 'ted': [at_user(user_id), create_text_msg(' 回合结束')],
+                }
                 return onbattle_dic[order[0]]
             else:
                 P.save_battle()
